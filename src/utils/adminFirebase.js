88ae5967e4ad69fdd4763e6e9be79a6e6058f219
Sappakit <pipitwattana.s@gmail.com>
2025-01-02 11:23:39 +0700

    fix: encode serviceAccountKey.json as base64 for vercel compatibility

--- src/utils/adminFirebase.js
+++ src/utils/adminFirebase.js
@@ -1,7 +1,11 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-  const serviceAccount = require("./serviceAccountKey.json");
+  const serviceAccount = JSON.parse(
+    Buffer.from(process.env.SERVICE_ACCOUNT_KEY_BASE64, "base64").toString(
+      "utf8",
+    ),
+  );
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
